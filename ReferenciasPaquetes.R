83e7487d8443a3f0f69dc995f2748a57b086bc66
Mariana <marianavelez.35@gmail.com>
2023-03-07 21:11:11 -0600

    References

---
+++ ReferenciasPaquetes.R
@@ -0,0 +1,7 @@
+#Grateful citations
+library(phyloseq)
+library(vegan)
+library(microbiome)
+library(BiocManager)
+library(grateful)
+cite_packages(output = "file",out.format = "docx",out.name = "referencias",out.dir = getwd(),citation.style = NULL )
